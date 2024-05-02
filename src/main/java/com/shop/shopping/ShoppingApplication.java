5bb0e8c6de97ad5a4cb7ba5f60594ffbfe6b5c8f
yongsun.jang <yongsun.jang@playd.com>
2024-05-02 22:13:14 +0900

    초기세팅

--- src/main/java/com/shop/shopping/ShoppingApplication.java
+++ src/main/java/com/shop/shopping/ShoppingApplication.java
@@ -2,12 +2,12 @@ package com.shop.shopping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class ShoppingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ShoppingApplication.class, args);
     }
-
 }
